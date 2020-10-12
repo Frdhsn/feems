39df67f77c9c396388b0fa4b5732ce6f214ac191
rana <ashikex49@gmail.com>
2020-10-12 14:10:59 +0600

    update static

--- feems/settings.py
+++ feems/settings.py
@@ -111,6 +111,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
